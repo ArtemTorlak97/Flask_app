1fdd0188eae81117dbf93959ba711760d8b64dbc
artemtorlak <artem-972007@mail.ru>
2018-04-28 22:49:59 +0300

    Add blueprint

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3.5
 from flask import Flask
 from config import Configuration
+#import value posts (object of class blueprint)
+from posts.blueprint import posts
 
 app = Flask(__name__) 
 app.config.from_object(Configuration)
+app.register_blueprint(posts, url_prefix='/blog')

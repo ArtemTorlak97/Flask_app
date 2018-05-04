959ee661d012b4a81989b4d5747dd21d817af655
artemtorlak <artem-972007@mail.ru>
2018-05-04 17:37:54 +0300

    Add full admin menu

--- app/app.py
+++ app/app.py
@@ -6,8 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-#import value posts (object of class blueprint)
+# import value posts (object of class blueprint)
 from flask_admin import Admin
+# import forms for admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -18,4 +20,8 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+#ADMIN TOOLS#
+from models import *
 admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@ class Configuration(object):
 	DEBUG = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://artem:123@localhost/test1'
+	SECRET_KEY = 'MY_SECRET'
